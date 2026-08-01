a6fa0410a46bceec5a2fa1499cc5152b56a32ad6
NITIN GUPTA <88649588+nitin28061999@users.noreply.github.com>
2026-08-01 18:57:54 +0530

    prediction

--- app/predictor.py
+++ app/predictor.py
@@ -1,1 +1,19 @@
-﻿
+﻿import joblib
+import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+model = joblib.load(BASE_DIR / "models" / "random_forest_model.pkl")
+columns = joblib.load(BASE_DIR / "models" / "model_columns.pkl")
+
+
+def predict_salary(user_input):
+    input_df = pd.DataFrame([user_input])
+
+    input_df = pd.get_dummies(input_df)
+
+    input_df = input_df.reindex(columns=columns, fill_value=0)
+
+    prediction = model.predict(input_df)
+
+    return prediction[0]
