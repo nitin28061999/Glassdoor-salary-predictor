4660d060815c57e5af4e7f7aa0f0373ed7710223
NITIN GUPTA <88649588+nitin28061999@users.noreply.github.com>
2026-08-01 19:00:16 +0530

    eda

---
+++ app/eda.py
@@ -0,0 +1,49 @@
+﻿import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("../data/processed/glassdoor_jobs_cleaned.csv")
+
+
+def salary_distribution():  # sourcery skip: inline-immediately-returned-variable
+
+    fig = px.histogram(
+        df,
+        x="Average Salary",
+        nbins=25,
+        title="Salary Distribution"
+    )
+
+    return fig
+
+
+def top_job_titles():  # sourcery skip: inline-immediately-returned-variable
+
+    top = (
+        df.groupby("Job Title")["Average Salary"]
+        .mean()
+        .sort_values(ascending=False)
+        .head(10)
+        .reset_index()
+    )
+
+    fig = px.bar(
+        top,
+        x="Average Salary",
+        y="Job Title",
+        orientation="h",
+        title="Top Paying Job Titles"
+    )
+
+    return fig
+
+
+def company_size_salary():  # sourcery skip: inline-immediately-returned-variable
+
+    fig = px.box(
+        df,
+        x="Size",
+        y="Average Salary",
+        title="Salary by Company Size"
+    )
+
+    return fig
