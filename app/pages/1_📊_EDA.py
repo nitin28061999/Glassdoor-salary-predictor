4e678af2444f0aea5af07b8e8ccd78d399ace48e
NITIN GUPTA <88649588+nitin28061999@users.noreply.github.com>
2026-08-01 20:34:27 +0530

    EDA_PAGE

---
+++ app/pages/1_📊_EDA.py
@@ -0,0 +1,13 @@
+﻿import streamlit as st
+from eda import *
+
+st.title("📊 Exploratory Data Analysis")
+
+st.subheader("Salary Distribution")
+st.plotly_chart(salary_distribution(), use_container_width=True)
+
+st.subheader("Top Paying Job Titles")
+st.plotly_chart(top_job_titles(), use_container_width=True)
+
+st.subheader("Salary by Company Size")
+st.plotly_chart(company_size_salary(), use_container_width=True)
