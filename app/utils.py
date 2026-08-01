bea4dbea9e4031af087b661d7a991e4c0e82ea4a
NITIN GUPTA <88649588+nitin28061999@users.noreply.github.com>
2026-08-01 19:01:19 +0530

    utility

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,23 @@
-﻿
+﻿import pandas as pd
+
+df = pd.read_csv("../data/processed/glassdoor_jobs_cleaned.csv")
+
+
+def get_job_titles():
+    return sorted(df["Job Title"].dropna().unique())
+
+
+def get_states():
+    return sorted(df["State"].dropna().unique())
+
+
+def get_industries():
+    return sorted(df["Industry"].dropna().unique())
+
+
+def get_sizes():
+    return sorted(df["Size"].dropna().unique())
+
+
+def get_sectors():
+    return sorted(df["Sector"].dropna().unique())
