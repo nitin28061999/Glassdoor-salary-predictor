8f483e81a9f18d95592ff721a9555adc039127e4
NITIN GUPTA <88649588+nitin28061999@users.noreply.github.com>
2026-08-01 19:02:21 +0530

    app

--- app/app.py
+++ app/app.py
@@ -1,1 +1,25 @@
-﻿
+﻿import streamlit as st
+
+from predictor import predict_salary
+
+from eda import *
+
+from utils import *
+
+st.set_page_config(
+    page_title="Glassdoor Salary Predictor",
+    layout="wide"
+)
+
+st.title("💼 Glassdoor Salary Prediction")
+
+st.sidebar.title("Navigation")
+
+page = st.sidebar.radio(
+    "Go To",
+    [
+        "Home",
+        "EDA",
+        "Predict Salary"
+    ]
+)
